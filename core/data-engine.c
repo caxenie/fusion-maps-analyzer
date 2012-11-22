6c836f49ad756fd45d282a00c2b0c3d8a797f8f1
Cristian Axenie <cristian.axenie@tum.de>
2012-11-22 23:54:57 +0100

    Various improvements. The 0.0 glitch not that prominent. Fixed timer types.

--- core/data-engine.c
+++ core/data-engine.c
@@ -268,7 +268,7 @@ void handle_client_signals(DBusMessage *msg)
         // invalidate the sensor connection
         sensor_connected[map_id] = 0;
         // start timer for data update rate if the button was pressed
-        if((rc=create_rate_timer(&user_timer[map_id], SYNC_DATA/US_TO_MS, SYNC_DATA/US_TO_MS, PERIODIC))==-1){
+        if((rc=create_rate_timer(&user_timer[map_id], SYNC_DATA/US_TO_MS, SYNC_DATA/US_TO_MS, ONE_SHOT))==-1){
             printf("Error setting timer for the user data update in map %d \n", map_id);
         }
         printf("created user timer for map %d\n", map_id);
@@ -292,7 +292,7 @@ void handle_client_signals(DBusMessage *msg)
             // invalidate sensor connection
             sensor_connected[map_id] = 0;
             // start timer for data update rate
-            if((rc=create_rate_timer(&user_timer[map_id], SYNC_DATA*(int)update_rate_user[map_id]/US_TO_MS, SYNC_DATA*(int)update_rate_user[map_id]/US_TO_MS, PERIODIC))==-1){
+            if((rc=create_rate_timer(&user_timer[map_id], SYNC_DATA*(int)update_rate_user[map_id]/US_TO_MS, SYNC_DATA*(int)update_rate_user[map_id]/US_TO_MS, ONE_SHOT))==-1){
                 printf("Error setting timer for the user data update in map %d \n", map_id);
             }
             printf("created user timer for map %d\n", map_id);
@@ -323,7 +323,7 @@ void handle_client_signals(DBusMessage *msg)
         }
         user_data[map_id] = data;
         // start timer for data update rate
-        if((rc=create_rate_timer(&user_timer[map_id], SYNC_DATA*(int)update_rate_user[map_id]/US_TO_MS, SYNC_DATA*(int)update_rate_user[map_id]/US_TO_MS, PERIODIC))==-1){
+        if((rc=create_rate_timer(&user_timer[map_id], SYNC_DATA*(int)update_rate_user[map_id]/US_TO_MS, SYNC_DATA*(int)update_rate_user[map_id]/US_TO_MS, ONE_SHOT))==-1){
             printf("Error setting timer for the user data update in map %d \n", map_id);
         }
         printf("created user timer for map %d\n", map_id);
