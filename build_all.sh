6eb95ea62abf622f215ad5efc262db44a5494492
Cristian Axenie <cristian.axenie@gmail.com>
2015-04-30 11:28:01 +0200

    Added 64bit OS compatibility. Stable version.

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo "Building Interacting Maps for Sensor Fusion Demo \n"
+rm fusion-analyzer-data.log
 cd core
 echo "Building core ...\n"
 make clean 
@@ -15,5 +16,8 @@ echo "Starting demo ...\n"
 mv gui/fusion_maps_gui .
 mv core/fusion_maps_core .
 ./fusion_maps_gui
+cd core/ && make clean 
+cd - && cd gui/ && make clean && cd -
+rm fusion_maps_gui fusion_maps_core
 
 
